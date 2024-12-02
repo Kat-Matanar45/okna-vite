4ca9f510b37b36311e2cdaa77300754c98f3dd77
Kat <avchimchik@gmail.com>
2024-12-03 01:42:33 +0500

    Модальное открывается и на крестик закрывается

--- src/js/modalTel.js
+++ src/js/modalTel.js
@@ -1,11 +1,16 @@
 export const modalTel = () => {
-    const btnTel = document.querySelector('.btn-md');
+    const btnTel = document.querySelectorAll('.button')[0];
     const modalWindow = document.querySelector('.header-modal');
     const substrate = document.querySelector('.overlay');
-
+    const btnClose = document.querySelector('.header-modal__close');
 
     btnTel.addEventListener('click', () => {
         modalWindow.style.display = 'block';
         substrate.style.display = 'block';
+    });
+    
+    btnClose.addEventListener('click', () => {
+        modalWindow.style.display = 'none';
+        substrate.style.display = 'none';
     })
 }
