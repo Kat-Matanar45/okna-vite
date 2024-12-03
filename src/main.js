4a6cb462397c8fa0af8de24bd3155f2ce304aba4
Kat <avchimchik@gmail.com>
2024-12-04 01:40:22 +0500

    Вторая модалка работает

--- src/main.js
+++ src/main.js
@@ -7,8 +7,10 @@ import 'swiper/css';
 import { modalTel } from './js/modalTel';
 import { sliderCarousel } from './js/sliderCarousel';
 import { serviceSlider } from './js/serviceSlider';
+import { modalService } from './js/modalService';
 
 modalTel();
 sliderCarousel();
 serviceSlider();
+modalService();
 
