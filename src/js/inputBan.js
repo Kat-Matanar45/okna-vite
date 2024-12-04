ea7850651160305599391fcd6b478bab8a7559af
Kat <avchimchik@gmail.com>
2024-12-05 00:51:23 +0500

    Баны инпутов работают

--- src/js/inputBan.js
+++ src/js/inputBan.js
@@ -2,5 +2,16 @@ export const inputBan = () => {
     const inputFio = document.querySelectorAll('input[name="fio"]');
     const inputPhone = document.querySelectorAll('input[name="phone"]');
 
+    inputFio.forEach((input) => {
+        input.addEventListener('input', (e) => {
+            e.target.value = e.target.value.replace(/[^а-яА-Яa-zA-Z]+/, '')
+        })
+    });
 
+    inputPhone.forEach((input) => {
+        input.addEventListener('input', (e) => {
+            e.target.value = e.target.value.replace(/[^\d\+]+/, '');
+            e.target.setAttribute("maxlength", "16");
+        })
+    });
 }
